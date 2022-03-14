c0c08e9fb908a1c0023852c15a623493e13d754c
saityan <naminatosky@gmail.com>
2022-03-14 13:22:24 +0300

    minor fix

--- app/src/main/java/ru/geekbrains/mvp/mvpuser/UserView.kt
+++ app/src/main/java/ru/geekbrains/mvp/mvpuser/UserView.kt
@@ -2,7 +2,6 @@ package ru.geekbrains.mvp.mvpuser
 
 import moxy.MvpView
 import moxy.viewstate.strategy.alias.SingleState
-import ru.geekbrains.mvp.data.GitHubUser
 import ru.geekbrains.mvp.data.GitHubUserRepositories
 
 interface UserView : MvpView {
